2f39621df8048ab343d9b9e2bc0439d478bda33a
Alex230707 <107807396+Alex230707@users.noreply.github.com>
2024-11-16 23:16:00 +0100

    Checkcollision.cs removed

--- Assets/Script/PlayerMovementScript.cs
+++ Assets/Script/PlayerMovementScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -10,52 +11,74 @@ public class PlayerMovementScript : MonoBehaviour
     private Vector2 MoveInput;
     private Vector2 MoveDirection;
     private Vector2 LastDirection;
+    private Vector2 NextDirection;
+    public bool IsOccupied;
+    public InputActionReference move;
+
     [SerializeField] private float MoveSpeed;
-
+    [SerializeField] private LayerMask ObstacleLayer;
     private void OnMovement(InputValue inputValue) {
-        MoveInput = inputValue.Get<Vector2>().normalized;
+        //MoveInput = inputValue.Get<Vector2>().normalized;
 
-        LockInputIn4Dir(ref MoveInput); /// Blocca il Movimento del Player nelle direzioni Sù, Giù, Destra, Sinistra evitando il movimento diagonale
-
-        if(MoveInput != Vector2.zero)
+        MoveDirection = move.action.ReadValue<Vector2>();
+        NextDirection = MoveDirection;
+        
+        LockMovementin4Dir(ref MoveDirection); /// Blocca il Movimento del Player nelle direzioni Sù, Giù, Destra, Sinistra evitando il movimento diagonale        
+        
+        if (MoveDirection != Vector2.zero)
         {
-             LastDirection = MoveInput;
+            LastDirection = MoveDirection;
         }
-
     }
+
 
 
 
     void Update()
-    {
-        Vector2 direction = MoveInput != Vector2.zero ? MoveInput : LastDirection;
+    {       
+            IsOccupied = Occupied(MoveDirection);
+            Vector2 direction = MoveDirection != Vector2.zero ? MoveDirection : LastDirection;
 
-        Vector3 Move = new Vector3(direction.x, direction.y) * MoveSpeed * Time.deltaTime; ///Fa traslare in Player
-        transform.Translate(Move);
+            Vector3 Move = new Vector3(direction.x, direction.y) * MoveSpeed * Time.deltaTime; ///Fa traslare in Player
+            transform.Translate(Move);
+
+        Debug.Log(IsOccupied);
     }
 
+        private void LockMovementin4Dir(ref Vector2 Input)
+        {
 
+            if (Input.y > 0.1f)
+            {
+                Input = new Vector2(0, 1);
+            }
 
-    private Vector2 LockInputIn4Dir(ref Vector2 Input)
+            else if (Input.y < -0.1f)
+            {
+                Input = new Vector2(0, -1);
+            }
+
+            else if (Input.x > 0.1f)
+            {
+                Input = new Vector2(1, 0);
+            }
+
+            else if (Input.x < -0.1f)
+            {
+                Input = new Vector2(-1, 0);
+            }
+        }
+
+    bool Occupied(Vector2 direction)
     {
-        if (Input.y > 0.1f)
-        {
-            Input = new Vector2(0, 1);
-        }
-
-        else if (Input.y < -0.1f)
-        {
-            Input = new Vector2(0, -1);
-        }
-
-        else if (Input.x > 0.1f)
-        {
-            Input = new Vector2(1, 0);
-        }
-        else if (Input.x < -0.1f)
-        {
-            Input = new Vector2(-1, 0);
-        }
-        return Input;
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.5f, 0.0f, MoveDirection, 1.5f, this.ObstacleLayer);
+        return hit.collider != null;
     }
-}
+    
+    void Stop()
+        {
+            MoveDirection = Vector2.zero;
+            LastDirection=Vector2.zero;
+
+        }
+    } 
